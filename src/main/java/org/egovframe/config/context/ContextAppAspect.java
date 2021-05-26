e748894aa9e011470d1136f081476d22741e1414
Jooho Yoon <platonicojju@gmail.com>
2021-05-26 09:51:09 +0900

    Refactor: exceptionTransfer에 DefaultExceptionHandleManager 등록
    TODO: Exception을 처리하는 방법 역할 별 정리 필요
    1) 비지니스 로직에서 AOP 사용해서 처리하는 경우
    2) Controller(Dispathcer Servlet)에서 Resolver로 처리하는 경우

--- src/main/java/org/egovframe/config/context/ContextAppAspect.java
+++ src/main/java/org/egovframe/config/context/ContextAppAspect.java
@@ -1,14 +1,17 @@
 package org.egovframe.config.context;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.AntPathMatcher;
 
 import egovframework.com.cmm.EgovComExcepHndlr;
 import egovframework.com.cmm.EgovComOthersExcepHndlr;
+import egovframework.rte.fdl.cmmn.aspect.ExceptionTransfer;
 import egovframework.rte.fdl.cmmn.exception.handler.ExceptionHandler;
 import egovframework.rte.fdl.cmmn.exception.manager.DefaultExceptionHandleManager;
+import egovframework.rte.fdl.cmmn.exception.manager.ExceptionHandlerService;
 
 @Configuration
 public class ContextAppAspect {
@@ -45,4 +48,20 @@ public class ContextAppAspect {
 		defaultExceptionHandleManager.setHandlers(new ExceptionHandler[] {otherHandler()});
 		return defaultExceptionHandleManager;
 	}
+
+	/**
+	 * @return
+	 * Exception 발생시 후처리를 위해 표준프레임워크 실행환경의 ExceptionTransfer를 활용하도록  설정
+	 */
+	@Bean
+	public ExceptionTransfer exceptionTransfer(
+		@Qualifier("defaultExceptionHandleManager") DefaultExceptionHandleManager defaultExceptionHandleManager,
+		@Qualifier("otherExceptionHandleManager") DefaultExceptionHandleManager otherExceptionHandleManager) {
+		ExceptionTransfer exceptionTransfer = new ExceptionTransfer();
+		exceptionTransfer.setExceptionHandlerService(new ExceptionHandlerService[] {
+			defaultExceptionHandleManager, otherExceptionHandleManager
+		});
+		return exceptionTransfer;
+	}
+
 }
