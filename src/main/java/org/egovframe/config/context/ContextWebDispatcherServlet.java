dccb43d6c91144cfbb2d16d64dd9de5840d060e8
Jooho Yoon <platonicojju@gmail.com>
2021-07-07 09:58:12 +0900

    Refactor: DispatcherServlet 변환
    Dispatcher Servlet 변환
    
    주의사항
    xml 부분적으로 지우면서 테스트시
    <mvc:annotation-driven/> 삭제 후 적용 필요
    
    WebMvcConfigurationSupport으로 재구성 하는것이므로 ..
    
    TODO 자세한 근거 필요

--- src/main/java/org/egovframe/config/context/ContextWebDispatcherServlet.java
+++ src/main/java/org/egovframe/config/context/ContextWebDispatcherServlet.java
@@ -1,13 +1,117 @@
 package org.egovframe.config.context;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
+import java.util.List;
+import java.util.Properties;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+import egovframework.com.cmm.interceptor.AuthenticInterceptor;
+
+/**
+ * @ClassName : ContextWebDispatcherServlet.java
+ * @Description :
+ *
+ * @author : 윤주호
+ * @since  : 2021. 7. 1
+ * @version : 1.0
+ *
+ * <pre>
+ * << 개정이력(Modification Information) >>
+ *
+ *   수정일              수정자               수정내용
+ *  -------------  ------------   ---------------------
+ *   2021. 7. 1    윤주호               최초 생성
+ * </pre>
+ *
+ */
 @Configuration
-@ImportResource(value= {
+/*@ImportResource(value = {
 	//"/WEB-INF/config/egovframework/springmvc/*.xml"
-	"/WEB-INF/config/egovframework/springmvc/egov-com-servlet.xml"
-	})
-public class ContextWebDispatcherServlet {
+	//"/WEB-INF/config/egovframework/springmvc/egov-com-servlet.xml"
+})*/
+@ComponentScan(basePackages = "egovframework", excludeFilters = {
+	@ComponentScan.Filter(type = FilterType.ANNOTATION, value = Service.class),
+	@ComponentScan.Filter(type = FilterType.ANNOTATION, value = Repository.class),
+	@ComponentScan.Filter(type = FilterType.ANNOTATION, value = Configuration.class)
+})
+public class ContextWebDispatcherServlet extends WebMvcConfigurationSupport {
+
+	final static String RESOLVER_DEFAULT_ERROR_VIEW = "cmm/error/egovError";
+
+	final static int URL_BASED_VIEW_RESOLVER_ORDER = 1;
+	final static String URL_BASED_VIEW_RESOLVER_PREFIX = "/WEB-INF/jsp/";
+	final static String URL_BASED_VIEW_RESOLVER_SUFFIX = ".jsp";
+
+	// =====================================================================
+	// RequestMappingHandlerMapping 설정
+	// =====================================================================
+	// -------------------------------------------------------------
+	// RequestMappingHandlerMapping 설정 - Interceptor 추가
+	// -------------------------------------------------------------
+	@Override
+	protected void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new AuthenticInterceptor())
+			.addPathPatterns(
+				"/cop/com/*.do",
+				"/cop/bbs/*Master*.do",
+				"/uat/uia/*.do")
+			.excludePathPatterns(
+				"/uat/uia/actionLogin.do",
+				"/uat/uia/egovLoginUsr.do");
+	}
+
+	// -------------------------------------------------------------
+	// RequestMappingHandlerMapping 설정 View Controller 추가
+	// -------------------------------------------------------------
+	@Override
+	protected void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/cmmn/validator.do")
+			.setViewName("cmmn/validator");
+	}
+
+	// -------------------------------------------------------------
+	// HandlerExceptionResolver 설정
+	// -------------------------------------------------------------
+	@Override
+	protected void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
+		SimpleMappingExceptionResolver simpleMappingExceptionResolver = new SimpleMappingExceptionResolver();
+
+		simpleMappingExceptionResolver.setDefaultErrorView(RESOLVER_DEFAULT_ERROR_VIEW);
+
+		Properties mappings = new Properties();
+		mappings.setProperty("org.springframework.dao.DataAccessException", "cmm/error/dataAccessFailure");
+		mappings.setProperty("org.springframework.transaction.TransactionException", "cmm/error/transactionFailure");
+		mappings.setProperty("egovframework.rte.fdl.cmmn.exception.EgovBizException", "cmm/error/egovError");
+		mappings.setProperty("org.springframework.security.AccessDeniedException", "cmm/error/accessDenied");
+
+		simpleMappingExceptionResolver.setExceptionMappings(mappings);
+
+		exceptionResolvers.add(simpleMappingExceptionResolver);
+	}
+
+	// -------------------------------------------------------------
+	// View Resolver 설정
+	// -------------------------------------------------------------
+	@Bean
+	public UrlBasedViewResolver urlBasedViewResolver() {
+		UrlBasedViewResolver urlBasedViewResolver = new UrlBasedViewResolver();
+		urlBasedViewResolver.setOrder(URL_BASED_VIEW_RESOLVER_ORDER);
+		urlBasedViewResolver.setViewClass(JstlView.class);
+		urlBasedViewResolver.setPrefix(URL_BASED_VIEW_RESOLVER_PREFIX);
+		urlBasedViewResolver.setSuffix(URL_BASED_VIEW_RESOLVER_SUFFIX);
+		return urlBasedViewResolver;
+	}
 
 }
